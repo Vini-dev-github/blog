944a382bdbd5a89a403827b77bd451aa88b978da
Vini <viniciuslima.profissional@gmail.com>
2024-08-07 16:16:27 -0300

    add function update post

--- posts_app/urls.py
+++ posts_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post-list'),
     path('post-create', views.create_post, name='post-create'),
-    path('post-detail/<int:id>/', views.post_details, name='post-detail'),   
+    path('post-detail/<int:id>/', views.post_details, name='post-detail'),
+    path('post-update/<int:id>/', views.post_update, name='post-update'),
 ]
--- posts_app/views.py
+++ posts_app/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404
 from posts_app.models import Posts
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from posts_app.forms import PostForm
 from django.contrib import messages
+from .forms import PostForm
 
 def post_list(request):
     template_name = 'post-list.html'
@@ -37,3 +38,14 @@ def post_details(request, id):
         'post': post
     }
     return render(request, template_name, context)
+
+def post_update(request, id):
+    post = get_object_or_404(Posts, id=id)
+    form = PostForm(request.POST or None, request.FILES or None, instance=post)
+    if form.is_valid():
+        form.save()
+
+        messages.success(request, 'Post atualizado com sucesso')
+        return HttpResponseRedirect(reverse('post-detail', args=[post.id]))
+    
+    return render(request, 'post-form.html', {'form': form})
