00a9e47037302240e0966f88988e0fdb645ef65f
Vini <viniciuslima.profissional@gmail.com>
2024-08-11 21:47:35 -0300

    Settings adjust

--- core_project/settings.py
+++ core_project/settings.py
@@ -128,3 +128,13 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+ }
