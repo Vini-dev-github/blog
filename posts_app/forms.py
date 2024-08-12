3c93c38b0e8a26b894945302e283e0a837483852
Vini <viniciuslima.profissional@gmail.com>
2024-08-11 21:41:01 -0300

    Added 'Atualizar' link to post-detail.html template

--- posts_app/forms.py
+++ posts_app/forms.py
@@ -5,3 +5,8 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Posts
         fields = ('title', 'description', 'image')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
